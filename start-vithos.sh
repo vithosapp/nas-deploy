44b172e98971736e006df95334cceead1686b54c
HrxSrv <harshitsrv2004@gmail.com>
2026-08-08 23:33:16 +0530

    update: paths

--- start-vithos.sh
+++ start-vithos.sh
@@ -5,4 +5,4 @@ sshd
 
 tmux new-session -d -s vitals 'cd ~/nas/vitals && pnpm start > ~/nas/vitals/server.log 2>&1'
 tmux new-session -d -s tunnel 'cloudflared tunnel run > ~/tunnel.log 2>&1'
-tmux new-session -d -s webhook 'webhook -hooks ~/nas-deploy/hooks.json -port 9000 -verbose > ~/nas-deploy/webhook.log 2>&1'
+tmux new-session -d -s webhook 'webhook -hooks ~/nas/nas-deploy/hooks.json -port 9000 -verbose > ~/nas/nas-deploy/webhook.log 2>&1'
